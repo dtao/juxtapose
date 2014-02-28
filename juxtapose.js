f7dd5654391a45fddc5990d2e97e36abe1dc8d80
Dan Tao <daniel.tao@gmail.com>
2014-02-28 07:25:55 -0800

    added closure-style docs (Google is infecting me!)

--- juxtapose.js
+++ juxtapose.js
@@ -1,9 +1,33 @@
 var stringify = require('json-stable-stringify');
 
-function Comparer(options) {
-  this.options = options || {};
+/**
+ * @typedef {{
+ *   separator: string=
+ * }}
+ */
+var Options;
+
+/**
+ * Class responsible for doing the actual comparison. The main reason to use a
+ * class for this is to have a place to store config options. Otherwise they'd
+ * have to be passed to the juxtapose method, which doesn't work very well when
+ * the method should also accept an arbitrary number of arguments.
+ *
+ * @constructor
+ * @param {Options=} opt_options
+ */
+function Comparer(opt_options) {
+  this.options = opt_options || {};
 }
 
+/**
+ * Compares two values (typically either strings or objects) and puts them side
+ * by side.
+ *
+ * @param {*} left
+ * @param {*} right
+ * @return {string}
+ */
 Comparer.prototype.compare = function compare(left, right) {
   var separator = this.options.separator || ' | ';
 
@@ -26,6 +50,13 @@ Comparer.prototype.compare = function compare(left, right) {
   return output.join('\n')
 };
 
+/**
+ * Compares two or more values using {@link Comparer#compare}.
+ *
+ * @param {*} left
+ * @param {*} right
+ * @return {string}
+ */
 function juxtapose(left, right) {
   var comparer = new Comparer();
 
@@ -37,6 +68,13 @@ function juxtapose(left, right) {
   return result;
 }
 
+/**
+ * Produces a 'juxtapose' function based on a {@link Comparer} constructed with
+ * the specified options.
+ *
+ * @param {Options} options
+ * @return {function(*...):string}
+ */
 juxtapose.withOptions = function withOptions(options) {
   var comparer = new Comparer(options);
 
@@ -45,17 +83,39 @@ juxtapose.withOptions = function withOptions(options) {
   };
 };
 
+/**
+ * Returns the length of the longest string in an array of strings.
+ *
+ * @private
+ * @param {Array.<string>} lines
+ * @return {number}
+ */
 function getLongestLine(lines) {
   return lines.reduce(function(maxLength, line) {
     return Math.max(maxLength, line.length);
   }, 0);
 }
 
+/**
+ * Pads a string to a desired width by appending whitespace.
+ *
+ * @private
+ * @param {string} string
+ * @param {number} width
+ * @return {string}
+ */
 function pad(string, width) {
   var padding = width - string.length;
   return string + space(padding);
 }
 
+/**
+ * Produces the desired amount of whitespace.
+ *
+ * @private
+ * @param {number} width
+ * @return {string}
+ */
 function space(width) {
   return Array(width + 1).join(' ');
 }
